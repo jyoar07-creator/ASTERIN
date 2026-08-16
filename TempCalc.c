8a21441ebbf6f1e5b79c3a96c65115135d05a2cc
jyoar07-creator <jyoar07@gmail.com>
2026-08-17 02:41:14 +0600

    Updated TempCalc.c to include new temperature conversion functions and added TempCalc.exe as the compiled executable for the program.

--- TempCalc.c
+++ TempCalc.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
-int main(){
+
+int main() {
+    int repeat;
     float C, F;
     char choice;
-    printf("What do you want to convert? (C/F): "); 
 
-    scanf(" %c", &choice);
-    if(choice == 'C' || choice == 'c'){
-        printf("Enter temperature in Celsius: ");
-        scanf("%f", &C);
-        F = (C * 9/5) + 32;
-        printf("%.2f Celsius is equal to %.2f Fahrenheit\n", C, F);
-    } else if(choice == 'F' || choice == 'f'){
-        printf("Enter temperature in Fahrenheit: ");
-        scanf("%f", &F);
-        C = (F - 32) * 5/9;
-        printf("%.2f Fahrenheit is equal to %.2f Celsius\n", F, C);
-    } else {
-        printf("Invalid choice. Please enter 'C' or 'F'.\n");
-    }
+    do {
+        printf("What do you want to convert? (C/F): "); 
+        scanf(" %c", &choice);
+        
+        if (choice == 'C' || choice == 'c') {
+            printf("Enter temperature in Celsius: ");
+            scanf("%f", &C);
+            F = (C * 9.0/5.0) + 32;
+            printf("%.2f Celsius is equal to %.2f Fahrenheit\n", C, F);
+        } else if (choice == 'F' || choice == 'f') {
+            printf("Enter temperature in Fahrenheit: ");
+            scanf("%f", &F);
+            C = (F - 32) * 5.0/9.0;
+            printf("%.2f Fahrenheit is equal to %.2f Celsius\n", F, C);
+        } else {
+            printf("Invalid choice. Please enter 'C' or 'F'.\n");
+        }
+        
+        // Ask the user if they want to run it again
+        printf("\nDo you want to perform another conversion? (1 for Yes, 0 for No): ");
+        scanf("%d", &repeat);
+        printf("\n"); // Just adds a blank line for readability
+
+    } while (repeat == 1);
+
+    printf("Goodbye!\n");
     return 0;
 }
